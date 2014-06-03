2667fd0240880906030506571dfd7b37e20b24b1
Dayoung Jung <dayoung25@daumcorp.com>
2014-06-03 18:10:16 +0900

    Initial Commit

---
+++ DownloaderSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DownloaderSample
+//
+//  Created by Dayoung on 2014. 6. 3..
+//  Copyright (c) 2014년 Dayoung Jung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DownloaderSampleTests/DownloaderSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  DownloaderSampleTests.m
+//  DownloaderSampleTests
+//
+//  Created by Dayoung on 2014. 6. 3..
+//  Copyright (c) 2014년 Dayoung Jung. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DownloaderSampleTests : XCTestCase
+
+@end
+
+@implementation DownloaderSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
